0633b44deb1badd3edbd92f3828e8b0407d3350c
Laurent <l.gabardos@gmail.com>
2018-03-23 15:22:31 +0100

    correct icon ressources + add fragment into MainActivity

---
+++ Android/app/src/main/java/fr/gabardos/mybrews/fragments/HomeFragment.java
@@ -0,0 +1,44 @@
+package fr.gabardos.mybrews.fragments;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.gabardos.mybrews.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment HomeFragment.
+     */
+    public static HomeFragment newInstance(String param1, String param2) {
+        return new HomeFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
---
+++ Android/app/src/main/java/fr/gabardos/mybrews/util/AppUtil.java
@@ -0,0 +1,11 @@
+package fr.gabardos.mybrews.util;
+
+/**
+ * Util class
+ * Created by Laurent on 23/03/2018.
+ */
+
+public class AppUtil {
+
+
+}
