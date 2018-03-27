6f33d6c8f27b791bda0aa0c009d2a35201acc272
Laurent <l.gabardos@gmail.com>
2018-03-27 11:11:26 +0200

    add constants file for the application

---
+++ Android/app/src/main/java/fr/gabardos/mybrews/util/Constants.java
@@ -0,0 +1,12 @@
+package fr.gabardos.mybrews.util;
+
+/**
+ *
+ * Created by Laurent on 27/03/2018.
+ */
+
+public class Constants {
+
+	public static String DATE_FORMAT_YMDHMS = "yyyy-MM-dd HH:mm:ss";
+
+}
